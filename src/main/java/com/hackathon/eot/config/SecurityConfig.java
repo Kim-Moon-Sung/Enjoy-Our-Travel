29d8b956907716bbdba436a2a002df0a53fa6d7c
MoonStar <moonsung0331@gmail.com>
2023-06-25 00:16:05 +0900

    #8 - webSecurityCustomizer 작성
    이전 스프링 시큐리티 버전에서는 WebSecurityConfigurerAdapter 를 통해서 configure 메소드를 통해서 구현하였던 부분을 최신 스프링 시큐리티에서 권장하는 SecurityFilterChain 을 통해서 구현하고 WebSecurity 부분을 Bean 으로 등록하는 방식으로 구현하였다.

--- src/main/java/com/hackathon/eot/config/SecurityConfig.java
+++ src/main/java/com/hackathon/eot/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,7 +17,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -32,11 +31,15 @@ public class SecurityConfig {
     }
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().regexMatchers("^(?!/api/).*")
+                .antMatchers(HttpMethod.POST, "/api/*/users/join", "/api/*/users/login");
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/*/users/join", "/api/*/users/login").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/*/users/join", "/api/*/users/login").permitAll()
                 .antMatchers("/api/**").authenticated()
                 .and()
                 .sessionManagement()
