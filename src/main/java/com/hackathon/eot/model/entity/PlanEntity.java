aea5b5e705440d38711100b2de6cc003a6cef8b7
MoonStar <moonsung0331@gmail.com>
2023-06-24 10:10:27 +0900

    #4 - PlannerEntity Jpa Auditing 추가

--- src/main/java/com/hackathon/eot/model/entity/PlanEntity.java
+++ src/main/java/com/hackathon/eot/model/entity/PlanEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @Table(name = "plan")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class PlanEntity {
+public class PlanEntity extends AuditingFields {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
