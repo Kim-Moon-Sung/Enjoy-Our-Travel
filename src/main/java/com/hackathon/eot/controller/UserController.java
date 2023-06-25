4f775ad44d3d89b3a7045bf31dd04f4352013a89
MoonStar <moonsung0331@gmail.com>
2023-06-25 11:13:11 +0900

    #10 - 유저 회원가입 기능 구현

---
+++ src/main/java/com/hackathon/eot/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.hackathon.eot.controller;
+
+import com.hackathon.eot.dto.request.UserJoinRequest;
+import com.hackathon.eot.dto.response.Response;
+import com.hackathon.eot.dto.response.UserJoinResponse;
+import com.hackathon.eot.model.dto.User;
+import com.hackathon.eot.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
+        User user = userService.join(request);
+        return Response.success(UserJoinResponse.fromUser(user));
+    }
+}
---
+++ src/main/java/com/hackathon/eot/service/UserService.java
@@ -0,0 +1,31 @@
+package com.hackathon.eot.service;
+
+import com.hackathon.eot.config.util.CustomUserService;
+import com.hackathon.eot.dto.request.UserJoinRequest;
+import com.hackathon.eot.exception.EotApplicationException;
+import com.hackathon.eot.exception.ErrorCode;
+import com.hackathon.eot.model.dto.User;
+import com.hackathon.eot.model.entity.UserAccount;
+import com.hackathon.eot.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final CustomUserService customUserService;
+    private final UserAccountRepository userAccountRepository;
+
+    @Transactional
+    public User join(UserJoinRequest request) {
+        userAccountRepository.findByUserAccountId(request.getUserAccountId()).ifPresent(it -> {
+            throw new EotApplicationException(ErrorCode.DUPLICATED_USER_ACCOUNT_ID, String.format("%s is duplicated", request.getUserAccountId()));
+        });
+
+        UserAccount user = userAccountRepository.save(UserAccount.of(request.getUserAccountId(), request.getPassword(),
+                request.getName(), request.getNickname(), request.getBirthday(), request.getGender(), request.getAddress()));
+        return User.fromEntity(user);
+    }
+}
