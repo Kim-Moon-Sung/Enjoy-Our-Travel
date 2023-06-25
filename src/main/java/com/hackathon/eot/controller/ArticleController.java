b272d6aa8ee4cb7828c0831c3f53135a7fbfc279
MoonStar <moonsung0331@gmail.com>
2023-06-26 01:09:20 +0900

    #14 - 댓글 작성 기능 구현

--- src/main/java/com/hackathon/eot/controller/ArticleController.java
+++ src/main/java/com/hackathon/eot/controller/ArticleController.java
@@ -1,13 +1,12 @@
 package com.hackathon.eot.controller;
 
 import com.hackathon.eot.dto.request.ArticleCreateRequest;
+import com.hackathon.eot.dto.request.PostCommentRequest;
 import com.hackathon.eot.dto.response.Response;
 import com.hackathon.eot.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/articles")
@@ -19,6 +18,12 @@ public class ArticleController {
     @PostMapping
     public Response<Void> create(ArticleCreateRequest request, Authentication authentication) throws Exception {
         articleService.create(request.getTitle(), request.getContent(), authentication.getName(), request.getFiles());
+        return Response.success();
+    }
+
+    @PostMapping("/{articleId}/comments")
+    public Response<Void> comment(@PathVariable Long articleId, @RequestBody PostCommentRequest request, Authentication authentication) {
+        articleService.comment(articleId, authentication.getName(), request.getContent());
         return Response.success();
     }
 }
---
+++ src/main/java/com/hackathon/eot/dto/request/PostCommentRequest.java
@@ -0,0 +1,11 @@
+package com.hackathon.eot.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PostCommentRequest {
+
+    String content;
+}
