5861b09fe4d7fbbf50a9d00a421e64dd3e2db1cb
MoonStar <moonsung0331@gmail.com>
2023-06-25 23:43:19 +0900

    #12 - 게시글 작성을 위한 엔티티 및 DTO 수정

--- src/main/java/com/hackathon/eot/model/dto/ImageDto.java
+++ src/main/java/com/hackathon/eot/model/dto/ImageDto.java
@@ -14,24 +14,30 @@ public class ImageDto {
 
     private Long id;
     private Long articleId;
-    private String imageUrl;
+    private String filePath;
+    private String originalFileName;
     private ImageRole role;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
 
-    public static ImageDto of(Long articleId, String imageUrl, ImageRole role) {
-        return new ImageDto(null, articleId, imageUrl, role, null, null);
+    public static ImageDto of(Long articleId, String filePath, String originalFileName, ImageRole role) {
+        return new ImageDto(null, articleId, filePath, originalFileName, role, null, null);
     }
 
-    public static ImageDto of(Long id, Long articleId, String imageUrl, ImageRole role, LocalDateTime createdAt, LocalDateTime modifiedAt) {
-        return new ImageDto(id, articleId, imageUrl, role, createdAt, modifiedAt);
+    public static ImageDto of(String filePath, String originalFileName, ImageRole role) {
+        return new ImageDto(null, null, filePath, originalFileName, role, null, null);
+    }
+
+    public static ImageDto of(Long id, Long articleId, String filePath, String originalFileName, ImageRole role, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        return new ImageDto(id, articleId, filePath, originalFileName, role, createdAt, modifiedAt);
     }
 
     public static ImageDto fromEntity(ImageEntity entity) {
         return new ImageDto(
                 entity.getId(),
                 entity.getArticle().getId(),
-                entity.getImageUrl(),
+                entity.getFilePath(),
+                entity.getOriginalFileName(),
                 entity.getRole(),
                 entity.getCreatedAt(),
                 entity.getModifiedAt()
--- src/main/java/com/hackathon/eot/model/entity/ArticleEntity.java
+++ src/main/java/com/hackathon/eot/model/entity/ArticleEntity.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Table(name = "article")
@@ -12,7 +14,8 @@ import javax.persistence.*;
 @Entity
 public class ArticleEntity extends AuditingFields {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
@@ -29,6 +32,11 @@ public class ArticleEntity extends AuditingFields {
     @Column(name = "content")
     private String content;
 
+    @OneToMany(mappedBy = "article",
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            orphanRemoval = true)
+    private List<ImageEntity> images = new ArrayList<>();
+
     private ArticleEntity(UserAccount userAccount, PlannerEntity plannerEntity, String title, String content) {
         this.user = userAccount;
         this.planner = plannerEntity;
@@ -36,7 +44,26 @@ public class ArticleEntity extends AuditingFields {
         this.content = content;
     }
 
+    public static ArticleEntity of(UserAccount userAccount, String title, String content) {
+        return new ArticleEntity(userAccount, null, title, content);
+    }
+
     public static ArticleEntity of(UserAccount userAccount, PlannerEntity plannerEntity, String title, String content) {
         return new ArticleEntity(userAccount, plannerEntity, title, content);
+    }
+
+    public void addImage(ImageEntity image) {
+        this.images.add(image);
+
+        // 게시글에 이미지 파일이 저장되어 있지 않는 경우
+        if(image.getArticle() != this)
+            image.setArticle(this);
+    }
+
+    public void setPlanner(PlannerEntity planner) {
+        this.planner = planner;
+
+        if(!planner.getArticles().contains(this))
+            planner.getArticles().add(this);
     }
 }
